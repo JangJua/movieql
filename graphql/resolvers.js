5f469cf5d70cbe6447a28b24f0bc02477b44cc0a
JangJua <jua0610@naver.com>
2020-10-14 17:10:46 +0900

    Creating first Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,12 @@
-import { getMovies, getById } from "./db"
+import { getMovies, getById, addMovie } from "./db"
 
 const resolvers = {
   Query: {
     movies: () => getMovies(),
     movie: (_, { id }) => getById(id)
   },
-  Mutations: {
-    addMovies: (_, { name, score }) =>
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score)
   }
 };
 
